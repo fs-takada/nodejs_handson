237276c2f7d4b6a7fe5974a13b479985825b83d3
r_takada <passion.engk@gmail.com>
2018-04-09 18:59:52 +0900

    fix: res.end()で処理を終わらせるように

--- for_gce0405.js
+++ for_gce0405.js
@@ -2,39 +2,49 @@ const https = require('https');
 const Compute = require('@google-cloud/compute');
 const compute = new Compute();
 
-const zone = compute.zone('asia-east1-a');
-const vm = zone.vm('xxx');
+const zone = compute.zone('***');
+const vm = zone.vm('***');
 const options = {
-  host: 'stg.xxx',
+  host: '***',
   rejectUnauthorized: false,
 };
 
+exports.auto_check_instance = (req, res) => {
 
-https.get(options, (res) => {
+  https.get(options, (url_res) => {
 
-  var statusCode = res.statusCode;
-  console.log(`statuscode: ${statusCode}`);
+    var statusCode = url_res.statusCode;
+    console.log(`statuscode: ${statusCode}`);
 
-  if (statusCode === 200) {
-    console.log('正常です');
-  } else {
-    console.log('インスタンスを再起動します。');
-    //vm.start();
-  };
+    if (statusCode !== 200) {
+      console.log('内部エラーが起きています。インスタンスを再起動させます。');
+      restart_instance();
+    };
+    res.end();
+  }).on('error', (e) => {
+    console.log('apacheかインスタンスが停止している可能性があるのでインスタンスを起動します。');
+    restart_instance();
+    res.end();
+  });
 
-}).on('error', (e) => {
-  console.log('Error code:'+ console.error(e) + '\nインスタンスを起動します。');
-  //インスタンスの状態を取得
+};
+
+function restart_instance() {
   vm.getMetadata(function(err, metadata, apiResponse) {
     console.log(metadata.status);
 
     if (metadata.status !== 'TERMINATED') {
       vm.stop();
       setTimeout(() => {
-        console.log('30秒たったよ')
+        console.log('インスタンスを停止させました。')
+        vm.start(function(err) {
+          if (!err) console.log('インスタンスを起動させました。')
+        });
       }, 30000);
+    } else {
+      vm.start(function(err) {
+        if (!err) console.log('インスタンスを起動させました。')
+      });
     }
-    vm.start();
   });
-
-});
+}
